1bfd5c9e727e22e812dde9f2cc15b8e1b213f235
RaidersOfTheARC <48812888+RaidersOfTheARC@users.noreply.github.com>
2019-03-25 12:10:04 -0400

    Update DriveTrain.java

--- DriveTrain.java
+++ DriveTrain.java
@@ -3,14 +3,13 @@
 /* Code by: Jackson Isenberg, Sahan Reddy, Shreya Das                         */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team6905.robot;
+package frc.robot;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import com.ctre.phoenix.motorcontrol.WPI_Talon;
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-import org.usfirst.frc.team6905.robot.RobotMap;
 
 /**
  *
@@ -26,19 +25,19 @@ public class DriveTrain {
     public DriveTrain() {
 		// constructor method for the drive train
 		
-		fLeft = new VictorSPX(RobotMap.DRIVETRAIN_LEFT_FRONT_VICTOR);
-		rLeft = new VictorSPX(RobotMap.DRIVETRAIN_LEFT_BACK_VICTOR);
+		fLeft = new WPI_VictorSPX(RobotMap.DRIVETRAIN_LEFT_FRONT_VICTOR);
+		rLeft = new WPI_VictorSPX(RobotMap.DRIVETRAIN_LEFT_BACK_VICTOR);
 		groupLeft = new SpeedControllerGroup(fLeft, rLeft);
 
-		fRight = new VictorSPX(RobotMap.DRIVETRAIN_RIGHT_FRONT_VICTOR);
-		rRight = new VictorSPX(RobotMap.DRIVETRAIN_RIGHT_BACK_VICTOR);
+		fRight = new WPI_VictorSPX(RobotMap.DRIVETRAIN_RIGHT_FRONT_VICTOR);
+		rRight = new WPI_VictorSPX(RobotMap.DRIVETRAIN_RIGHT_BACK_VICTOR);
 		groupRight = new SpeedControllerGroup(fRight, rRight);
 
 		drive = new DifferentialDrive(groupLeft, groupRight);
 
     }
 	
-	static void strafeDrive(double x, double y) {
+	public void strafeDrive(double x, double y) {
 
 		// left x and up y -> up left diagonal
 		if (x < 0 && y > 0) {
@@ -64,6 +63,10 @@ public class DriveTrain {
 			rRight.set(-x);
 		}
 		
+	}
+
+	public DifferentialDrive getDrive() {
+		return drive;
 	}
     
 }
